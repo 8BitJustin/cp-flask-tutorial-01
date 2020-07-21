d9193b470a50f319c5d11544bc08cb18b8c11c1a
8bitjustin <j.olson.digital@gmail.com>
2020-07-21 08:39:48 -0700

    Playing with strings and ints within urls.

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/home/<string:name>')
-def hello(name):
-    return f'Hello, {name}!'
+@app.route('/home/users/<string:name>/posts/<int:id>')
+def hello(name, id):
+    return f'Hello, {name}, you are viewing post {id}.'
 
 
 if __name__ == "__main__":
