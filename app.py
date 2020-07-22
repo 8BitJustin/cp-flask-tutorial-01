40e19ba732c14f2aa10ef74bfe4f129639a77f96
8bitjustin <j.olson.digital@gmail.com>
2020-07-21 21:32:23 -0700

    Created posts.html. Added all_posts list of dictionaries within app.py. Using for loop within posts.html to loop through both posts within all_posts.

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is content of post 1.'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is content of post 2.'
+    }
+]
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
 
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
