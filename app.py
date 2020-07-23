15538376d4e2e8fc3e5063a0928954453091a3b1
8bitjustin <j.olson.digital@gmail.com>
2020-07-23 07:45:31 -0700

    Created BlogPost class within app.py.

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-#53:53
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_created = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + self.id
+
 
 all_posts = [
     {
