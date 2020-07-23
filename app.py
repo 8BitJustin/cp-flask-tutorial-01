7591c2a61e5df0783d7b47d615584fe41009643e
8bitjustin <j.olson.digital@gmail.com>
2020-07-23 10:49:36 -0700

    Removed jibberish code from app.py.

--- app.py
+++ app.py
@@ -43,15 +43,5 @@ def posts():
     return render_template('posts.html', posts=all_posts)
 
 
-@app.route('/home/users/<string:name>/posts/<int:id>')
-def hello(name, id):
-    return f'Hello, {name}, you are viewing post {id}.'
-
-
-@app.route('/onlyget', methods=['GET'])
-def only_get():
-    return "You can only get this webpage"
-
-
 if __name__ == "__main__":
     app.run(debug=True)
